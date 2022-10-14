92cbd143300dd2a17c565f9f26fe5947758fccad
philipp <philipp@steckenbauer.com>
2022-10-14 12:42:09 +0400

    Added trimFront Method

--- SGN-Parser/Util/Util.class.js
+++ SGN-Parser/Util/Util.class.js
@@ -10,8 +10,19 @@ class Util {
 			return false;
 	}
 
+	static cloneJSON(data) {
+		if(data === null || typeof data === 'boolean' || Number.isFinite(data) || typeof data === 'string')
+			return data;
+		else if(Array.isArray(data))
+			return data.reduce((result, item) => [...result, Util.cloneJSON(item)], []);
+		else if(Object.prototype.toString.call(data) === '[object Object]')
+			return Object.entries(data).reduce((result, [key, value]) => ({...result, [key]: Util.cloneJSON(value)}), {});
+		else
+			throw new TypeError('Expected data to be valid JSON');
+	}
+
 	static trimFront(codeString) {
-		return codeString.replace(/^\s+/, '');
+		return codeString.replace(/^\s+/u, '');
 	}
 }
 
