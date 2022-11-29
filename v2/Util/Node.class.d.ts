77570799cdb0f71a46297b29a962a3485eb14dc8
philipp <philipp@steckenbauer.com>
2022-11-29 22:15:05 +0400

    Updated Nodes

---
+++ v2/Util/Node.class.d.ts
@@ -0,0 +1,30 @@
+import * as Util from './Util.class';
+import * as Range from './Range.class';
+import * as Location from './Location.class';
+
+export interface NodeJSON {
+	type: string;
+	data: Util.JSON_T;
+	range: Range.RangeJSON;
+	location: Location.LocationJSON;
+}
+
+export class Node {
+	static createNode(precedingNode: Node | null, type: string, data: Util.JSON_T, codeString: string): Node;
+
+	constructor(type: string, data: unknown, range: Range.Range, location: Location.Location);
+
+	get type(): string;
+
+	get data(): Util.JSON_T;
+
+	get range(): Range.Range;
+
+	get location(): Location.Location;
+
+	clone(): Node;
+
+	toJSON(): NodeJSON;
+
+	toString(): string;
+}
--- v2/Util/Node.class.js
+++ v2/Util/Node.class.js
@@ -1,4 +1,6 @@
 const Util = require('./Util.class');
+const Range = require('./Range.class');
+const Location = require('./Location.class');
 
 class Node {
 	#type;
@@ -6,11 +8,33 @@ class Node {
 	#range;
 	#location;
 
+	static createNode(precedingNode, type, data, codeString) {
+		if(typeof codeString !== 'string')
+			throw new TypeError('Expected codeString to be a string');
+
+		if(precedingNode instanceof Node) 
+			return new Node(
+				type,
+				data,
+				new Range(precedingNode.range.end + 1, precedingNode.range.end + codeString.length),
+				Location.calculateLocation(precedingNode.location, codeString)
+			);
+		else if(precedingNode === null)
+			return new Node(
+				type,
+				data,
+				new Range(0, codeString.length - 1),
+				Location.calculateLocation(null, codeString)
+			);
+		else
+			throw new TypeError('Expected precedingNode to be null or an instance of Node');
+	}
+
 	constructor(type, data, range, location) {
 		if (typeof type !== 'string')
 			throw new TypeError('Expected type to be a string');
 
-		if (!JSONUtil.isJSON(data))
+		if (!Util.isJSON(data))
 			throw new TypeError('Expected data to be valid JSON');
 
 		if (!(range instanceof Range))
@@ -39,6 +63,10 @@ class Node {
 
 	get location() {
 		return this.#location;
+	}
+
+	clone() {
+		return new Node(this.#type, Util.cloneJSON(this.#data), this.#range.clone(), this.#location.clone());
 	}
 
 	toJSON() {
