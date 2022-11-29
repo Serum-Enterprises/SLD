779308d577a19b696ef3075ef44ccb869145721f
philipp <philipp@steckenbauer.com>
2022-11-29 21:56:53 +0400

    Updated Util Class

--- v2/Util/Util.class.js
+++ v2/Util/Util.class.js
@@ -1,15 +1,17 @@
+class CloneError extends TypeError { };
+
 class Util {
 	static get CloneError() {
-		return class CloneError extends TypeError {};
+		return CloneError;
 	}
 
 	static isJSON(data) {
 		if (data === null || typeof data === 'boolean' || Number.isFinite(data) || typeof data === 'string')
 			return true;
 		else if (Array.isArray(data))
-			return data.every(JSONUtil.isJSON);
+			return data.every(Util.isJSON);
 		else if (Object.prototype.toString.call(data) === '[object Object]')
-			return Object.values(data).every(JSONUtil.isJSON);
+			return Object.values(data).every(Util.isJSON);
 		else
 			return false;
 	}
@@ -19,34 +21,28 @@ class Util {
 			return data;
 		else if (Array.isArray(data))
 			return data.reduce((result, item) => {
-				return [...result, JSONUtil.cloneJSON(item)];
+				return [...result, Util.cloneJSON(item)];
 			}, []);
 		else if (Object.prototype.toString.call(data) === '[object Object]')
 			return Object.entries(data).reduce((result, [key, value]) => {
-				return { ...result, [key]: JSONUtil.cloneJSON(value) };
+				return { ...result, [key]: Util.cloneJSON(value) };
 			}, {});
 		else
-			throw new Util.CloneError('Expected data to be valid JSON');
+			throw new CloneError('Expected data to be valid JSON');
 	}
 
-	// Print a String with Index Numbers below the Characters starting with based
-	static printIndex(s, based = 0) {
-		if(typeof s !== 'string')
-			throw new TypeError('Expected s to be a String');
-
-		if(!Number.isSafeInteger(based))
-			throw new TypeError('Expected based to be an Integer');
-
-		console.log(
-			s.split('')
-				.reduce((result, c, i) => {
-					return [
-						`${result[0]} ${padStart(c, `${i + based}`.length)}`,
-						`${result[1]} ${i + based}`
-					]
-				}, ['', ''])
-				.join('\n')
-		);
+	static printCharTable(str) {
+		console.table(str.split(/\r?\n/).map(line => line.split('')).reduce((result, line, i) => {
+			return {
+				...result,
+				[i]: line.reduce((result, char, j) => {
+					return {
+						...result,
+						[j]: char
+					};
+				}, {})
+			};
+		}, {}));
 	}
 }
 
