d41ac0705075b29eb5cdd4e3d4c2811287cc2340
philipp <philipp@steckenbauer.com>
2022-12-05 20:42:28 +0400

    Added JSON to Debug

--- Util/debug.js
+++ Util/debug.js
@@ -1,3 +1,37 @@
+class CloneError extends TypeError { };
+
+class Util {
+	static get CloneError() {
+		return CloneError;
+	}
+
+	static isJSON(data) {
+		if (data === null || typeof data === 'boolean' || Number.isFinite(data) || typeof data === 'string')
+			return true;
+		else if (Array.isArray(data))
+			return data.every(Util.isJSON);
+		else if (Object.prototype.toString.call(data) === '[object Object]')
+			return Object.values(data).every(Util.isJSON);
+		else
+			return false;
+	}
+
+	static cloneJSON(data) {
+		if (data === null || typeof data === 'boolean' || Number.isFinite(data) || typeof data === 'string')
+			return data;
+		else if (Array.isArray(data))
+			return data.reduce((result, item) => {
+				return [...result, Util.cloneJSON(item)];
+			}, []);
+		else if (Object.prototype.toString.call(data) === '[object Object]')
+			return Object.entries(data).reduce((result, [key, value]) => {
+				return { ...result, [key]: Util.cloneJSON(value) };
+			}, {});
+		else
+			throw new CloneError('Expected data to be valid JSON');
+	}
+}
+
 class Debug {
 	#namespace;
 
@@ -20,8 +54,12 @@ class Debug {
 			message.split('\n').forEach(line => console.log(`[${this.#namespace}] ${line}`));
 		else if(message instanceof Error)
 			message.stack.split('\n').forEach(line => console.log(`[${this.#namespace}] ${line}`));
+		else if(Util.isJSON(message))
+			JSON.stringify(message, null, 4)
+				.split('\n')
+				.forEach(line => console.log(`[${this.#namespace}] ${line}`));
 		else
-			throw new TypeError('Expected message to be a String or an instance of Error');
+			throw new TypeError('Expected message to be a String, JSON or an instance of Error');
 	}
 
 	warn(message) {
@@ -29,8 +67,12 @@ class Debug {
 			message.split('\n').forEach(line => console.warn(`[${this.#namespace}] ${line}`));
 		else if(message instanceof Error)
 			message.stack.split('\n').forEach(line => console.warn(`[${this.#namespace}] ${line}`));
+		else if(Util.isJSON(message))
+			JSON.stringify(message, null, 4)
+				.split('\n')
+				.forEach(line => console.warn(`[${this.#namespace}] ${line}`));
 		else
-			throw new TypeError('Expected message to be a String or an instance of Error');
+			throw new TypeError('Expected message to be a String, JSON or an instance of Error');
 	}
 
 	error(message) {
@@ -38,7 +80,13 @@ class Debug {
 			message.split('\n').forEach(line => console.error(`[${this.#namespace}] ${line}`));
 		else if(message instanceof Error)
 			message.stack.split('\n').forEach(line => console.error(`[${this.#namespace}] ${line}`));
+		else if(Util.isJSON(message))
+			JSON.stringify(message, null, 4)
+				.split('\n')
+				.forEach(line => console.error(`[${this.#namespace}] ${line}`));
 		else
-			throw new TypeError('Expected message to be a String or an instance of Error');
+			throw new TypeError('Expected message to be a String, JSON or an instance of Error');
 	}
 }
+
+module.exports = Debug;
