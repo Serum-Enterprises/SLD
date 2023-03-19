f59e102245597eaf76789492627331196e95c726
philipp <philipp@steckenbauer.com>
2023-03-19 17:05:29 +0400

    Grouped Examples and Added Parser Transformation

--- src/demo.ts
+++ src/demo.ts
@@ -1,38 +1,69 @@
-import { Rule, RuleVariant, Parser } from './index';
+import { Rule, RuleVariant, Parser, Result, Node, Meta } from './index';
 
-const ruleResult = Rule.begin('Hello', 'firstWord')
-	.directlyFollowedBy(/^\s+/, null, true)
-	.directlyFollowedBy('World', 'secondWord')
-	.execute('Hello\r\nWorld TEst', null);
+{
+	const ruleResult = Rule.begin('Hello', 'firstWord')
+		.directlyFollowedBy(/^\s+/, null, true)
+		.directlyFollowedBy('World', 'secondWord')
+		.execute('Hello\r\nWorld TEst', null);
 
-const variantResult = RuleVariant.create([
-	// English
-	Rule.begin('Hello', 'firstWord')
-		.followedBy('World', 'secondWord'),
-	// German
-	Rule.begin('Hallo', 'firstWord')
-		.followedBy('Welt', 'secondWord')
-]).execute('Hello World', null);
-
-const parserResult = Parser.create(
-	RuleVariant.create([
+	console.group('Testing a single Rule')
+	console.log('Result:\n', JSON.stringify(ruleResult, null, 2));
+	console.groupEnd();
+}
+{
+	const variantResult = RuleVariant.create([
 		// English
 		Rule.begin('Hello', 'firstWord')
 			.followedBy('World', 'secondWord'),
 		// German
 		Rule.begin('Hallo', 'firstWord')
 			.followedBy('Welt', 'secondWord')
-	])
-).parse('Hallo Welt', true);
+	]).execute('Hello World', null);
 
-console.group('Testing a single Rule')
-console.log('Result:\n', JSON.stringify(ruleResult, null, 2));
-console.groupEnd();
+	console.group('Testing a RuleVariant')
+	console.log('Result:\n', JSON.stringify(variantResult, null, 2));
+	console.groupEnd();
+}
+{
+	const parserResult = Parser.create(
+		RuleVariant.create([
+			// English
+			Rule.begin('Hello', 'firstWord')
+				.followedBy('World', 'secondWord'),
+			// German
+			Rule.begin('Hallo', 'firstWord')
+				.followedBy('Welt', 'secondWord')
+		])
+	).parse('Hallo Welt', true);
 
-console.group('Testing a RuleVariant')
-console.log('Result:\n', JSON.stringify(variantResult, null, 2));
-console.groupEnd();
+	console.group('Testing a Parser')
+	console.log('Result:\n', JSON.stringify(parserResult, null, 2));
+	console.groupEnd();
+}
 
-console.group('Testing a Parser')
-console.log('Result:\n', JSON.stringify(parserResult, null, 2));
-console.groupEnd();
+{
+	const parserResult = Parser.create(
+		RuleVariant.create([
+			// English
+			Rule.begin('Hello', 'firstWord')
+				.followedBy('World', 'secondWord')
+				.transform((nodes: { [key: string]: Node.Node }, raw: string, meta: Meta.Meta) => {
+					return Object.values(nodes).reduce((result, node) => {
+						return result + node.raw;
+					}, '');
+				}),
+			// German
+			Rule.begin('Hallo', 'firstWord')
+				.followedBy('Welt', 'secondWord')
+				.transform((nodes: { [key: string]: Node.Node }, raw: string, meta: Meta.Meta) => {
+					return Object.values(nodes).reduce((result, node) => {
+						return result + node.raw;
+					}, '');
+				}),
+		])
+	).parse('Hallo\r\n\nWelt');
+
+	console.group('Testing a Parser with transform')
+	console.log('Result:\n', JSON.stringify(parserResult, null, 2));
+	console.groupEnd();
+}
