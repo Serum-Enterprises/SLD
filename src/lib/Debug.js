171c1c3006c8516b5e54d5b266301a2a617cb39a
philipp <philipp@steckenbauer.com>
2023-07-22 16:20:20 +0200

    Added a Debugging Library

---
+++ src/lib/Debug.js
@@ -0,0 +1,58 @@
+class Debug {
+	static #lastTime;
+	#namespace;
+
+	constructor(namespace) {
+		if (typeof namespace !== 'string')
+			throw new TypeError('Expected namespace to be a String');
+
+		this.#namespace = namespace;
+	}
+
+	#diff() {
+		if (!Debug.#lastTime) {
+			Debug.#lastTime = process.hrtime.bigint();
+
+			return null;
+		}
+		else {
+			const diff = process.hrtime.bigint() - Debug.#lastTime;
+			Debug.#lastTime = process.hrtime.bigint();
+
+			return `${diff / 1000000n} ms`
+		}
+	}
+
+	log(message) {
+		if (typeof message !== 'string')
+			throw new TypeError('Expected message to be a String');
+
+		const diff = this.#diff();
+
+		console.log(`[${this.#namespace}] ${message}${diff ? ` +${diff}` : ''}`);
+	}
+
+	warn(message) {
+		if (typeof message !== 'string')
+			throw new TypeError('Expected message to be a String');
+
+		const diff = this.#diff();
+
+		console.warn(`[${this.#namespace}] ${message}${diff ? ` +${diff}` : ''}`);
+	}
+
+	error(message) {
+		if (typeof message !== 'string')
+			throw new TypeError('Expected message to be a String');
+
+		const diff = this.#diff();
+
+		console.error(`[${this.#namespace}] ${message}${diff ? ` +${diff}` : ''}`);
+	}
+
+	extend(namespace) {
+		return new Debug(`${this.#namespace}:${namespace}`);
+	}
+}
+
+module.exports = Debug;
