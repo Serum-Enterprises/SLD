678c959cf18c74a26fb78fc39b76160c0441fd2d
philipp <philipp@steckenbauer.com>
2023-08-21 14:41:53 +0200

    Added Unit Tests for Rule

---
+++ test/Rule.test.ts
@@ -0,0 +1,54 @@
+import { Component, ComponentInterface } from '../src/Component';
+import { RuleBuilder, Rule, RuleInterface } from '../src/Rule';
+
+describe("Testing Rule", () => {
+	test('Testing static fromJSON', () => {
+		const rule: RuleInterface = {
+			components: [
+				{
+					type: 'STRING',
+					value: 'testValue',
+					name: 'testName',
+					greedy: true,
+					optional: false
+				}
+			],
+			throwMessage: 'testThrowMessage',
+			recoverComponent: null
+		};
+
+		expect(Rule.fromJSON(rule)).toBeInstanceOf(Rule);
+	});
+
+	test('Testing constructor', () => {
+		const component = new Component('STRING', 'testValue', 'testName', true, false);
+
+		expect(new Rule([component], 'testThrowMessage', null)).toBeInstanceOf(Rule);
+	});
+
+	test('Testing addComponent', () => {
+		const component = new Component('STRING', 'testValue', 'testName', true, false);
+
+		expect(new Rule().addComponent(component)).toBeInstanceOf(Rule);
+	});
+
+	test('Testing setThrowMessage', () => {
+		expect(new Rule().setThrowMessage('testThrowMessage')).toBeInstanceOf(Rule);
+	});
+
+	test('Testing setRecoverComponent', () => {
+		const component = new Component('STRING', 'testValue', 'testName', true, false);
+
+		expect(new Rule().setRecoverComponent(component)).toBeInstanceOf(Rule);
+	});
+
+	test('Testing toJSON', () => {
+		const component = new Component('STRING', 'testValue', 'testName', true, false);
+
+		expect(new Rule([component], 'testThrowMessage', null).toJSON()).toStrictEqual({
+			components: [component.toJSON()],
+			throwMessage: 'testThrowMessage',
+			recoverComponent: null
+		});
+	});
+});
