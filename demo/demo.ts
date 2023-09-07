7ba6e9adb18059c66e7251973fd13ed075503323
philipp <philipp@steckenbauer.com>
2023-09-07 16:35:40 +0400

    Removed Demo

--- demo/demo.ts
+++
@@ -1,9 +0,0 @@
-import { grammar } from './expression';
-
-const source = '1 * 2 + 3 / 4';
-
-console.log(`Source is: ${source}`);
-console.log(`Source length is: ${source.length}`);
-
-// Expecting Output {}
-console.log(JSON.stringify(grammar.parse(source, 'expression').toJSON(), null, 2));
--- demo/parse.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const grammar = require('./expression');
-console.log(JSON.stringify(grammar.parse('1 + 1', 'expression').toJSON(), null, 2));
