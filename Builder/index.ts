c97f159ce0d7e34952bc6f6e7c01b46794f971b7
philipp <philipp@steckenbauer.com>
2023-11-24 10:47:33 +0400

    Updated index.ts with new exports

--- Builder/index.ts
+++ Builder/index.ts
@@ -1,19 +1,17 @@
-import { Component, ComponentInterface } from './src/Component';
-import { PrefixComponent, PrefixComponentInterface } from './src/Component';
-import { RecoverComponent, RecoverComponentInterface } from './src/Component';
+import { BaseComponent, BaseComponentInterface } from './src/Component';
+import { ComponentSet, ComponentSetInterface } from './src/Component';
 import { Rule, RuleInterface } from './src/Rule';
 import { Variant, VariantInterface } from './src/Variant';
 import { Grammar, GrammarInterface } from './src/Grammar';
 
 export {
-	PrefixComponent, PrefixComponentInterface,
-	RecoverComponent, RecoverComponentInterface,
-	Component, ComponentInterface,
+	BaseComponent, BaseComponentInterface,
+	ComponentSet, ComponentSetInterface,
 	Rule, RuleInterface,
 	Variant, VariantInterface,
 	Grammar, GrammarInterface
 };
 
 export default {
-	Component, Rule, Variant, Grammar
+	BaseComponent, ComponentSet, Rule, Variant, Grammar
 };
