0da2ad6840f08c4a088726518eb388c42c8de984
philipp <philipp@steckenbauer.com>
2024-01-03 22:41:25 +0100

    Resolved 2 Comparison Bugs

--- Parser/lib/dataClass/BaseSymbol.js
+++ Parser/lib/dataClass/BaseSymbol.js
@@ -7,7 +7,7 @@ class BaseSymbol {
 		if (typeof name !== 'string')
 			throw new TypeError('Expected name to be a String');
 
-		if (!Object.prototype.toString.call(data) === '[object Object]')
+		if (Object.prototype.toString.call(data) !== '[object Object]')
 			throw new TypeError(`Expected ${name} to be an Object`);
 
 		if (typeof data.type !== 'string')
@@ -19,7 +19,7 @@ class BaseSymbol {
 		if (typeof data.value !== 'string')
 			throw new TypeError(`Expected ${name}.value to be a String`);
 
-		if (typeof data.name !== 'string' && data.name !== null)
+		if (!(typeof data.name === 'string' || data.name === null))
 			throw new TypeError(`Expected ${name}.name to be a String or null`);
 
 		return new BaseSymbol(data.type, data.value, data.name);
