48ccb67eb0d565f007df84c9656ad3529fc41b9f
philipp <philipp@steckenbauer.com>
2024-01-06 16:21:59 +0100

    Removed old Parser Code

--- Parser/index.ts
+++
@@ -1,9 +0,0 @@
-import { Rule } from './src/Rule';
-import { Variant } from './src/Variant';
-import { Grammar } from './src/Grammar';
-
-export {
-	Rule, Variant, Grammar
-};
-
-export default Grammar;
