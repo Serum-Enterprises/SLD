e24385d80c6fb2488c8e1e763fde5fbb9aa0d11f
philipp <philipp@steckenbauer.com>
2024-01-16 10:52:21 +0100

    Added index.js to Builder

---
+++ Builder/index.js
@@ -0,0 +1,4 @@
+const { Rule, QuantitySelector, SymbolSelector } = require('./src/Rule');
+const { Grammar } = require('./src/Grammar');
+
+module.exports = { Rule, Grammar, QuantitySelector, SymbolSelector };
