ef9aca5d109c7f760b666060cb0ff3cebff8e981
philipp <philipp@steckenbauer.com>
2024-01-17 22:05:18 +0100

    Removed duplicate Constructor functionality and updated Imports

--- Builder/src/Grammar.js
+++ Builder/src/Grammar.js
@@ -1,5 +1,5 @@
-const { Grammar: GrammarDC } = require('../../../lib/src/Grammar');
-const { Rule: RuleDC } = require('../../../lib/src/Rule');
+const { Grammar: GrammarDC } = require('../../lib/src/Grammar');
+const { Rule: RuleDC } = require('../../lib/src/Rule');
 
 class Grammar extends GrammarDC {
 	/**
@@ -9,28 +9,6 @@ class Grammar extends GrammarDC {
 	 */
 	static create(ruleSets = {}) {
 		return new Grammar(ruleSets);
-	}
-
-	/**
-	 * Create a new Grammar Instance
-	 * @param {{[key: string]: RuleDC[]}} ruleSets 
-	 * @returns {Grammar}
-	 */
-	constructor(ruleSets = {}) {
-		if (!Object.prototype.toString.call(ruleSets) === '[object Object]')
-			throw new TypeError('Expected ruleSets to be an Object');
-
-		Object.entries(ruleSets).forEach(([name, ruleSet]) => {
-			if (!Array.isArray(ruleSet))
-				throw new TypeError(`Expected ruleSets.${name} to be an Array`);
-
-			ruleSet.forEach((rule, index) => {
-				if (!(rule instanceof RuleDC))
-					throw new TypeError(`Expected ruleSets.${name}[${index}] to be an instance of Rule`);
-			});
-		});
-
-		super(ruleSets);
 	}
 }
 
