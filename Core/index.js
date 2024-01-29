8dc1ddd7cf3971c835b751d410e02d527e56d120
philipp <philipp@steckenbauer.com>
2024-01-29 11:16:24 +0400

    Added Node to Core index

--- Core/index.js
+++ Core/index.js
@@ -3,5 +3,6 @@ const { RuleSet } = require('./src/RuleSet');
 const { Rule } = require('./src/Rule');
 const { SymbolSet } = require('./src/SymbolSet');
 const { BaseSymbol } = require('./src/BaseSymbol');
+const { Node } = require('./src/Node');
 
-module.exports = { Grammar, RuleSet, Rule, SymbolSet, BaseSymbol };
+module.exports = { Grammar, RuleSet, Rule, SymbolSet, BaseSymbol, Node };
