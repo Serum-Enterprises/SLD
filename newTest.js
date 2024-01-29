f6c39b87690bae0542ffe8bb8dd1c9a87791fe75
philipp <philipp@steckenbauer.com>
2024-01-29 13:15:21 +0400

    Updated Test File

--- newTest.js
+++ newTest.js
@@ -1,5 +1,5 @@
 const { Grammar, RuleSet, Rule } = require('./Builder');
-const { Parser } = require('./Parser');
+const { Parser, Node } = require('./Parser');
 
 const grammar = Grammar.create({
 	sum: RuleSet.create([
@@ -7,12 +7,14 @@ const grammar = Grammar.create({
 			.followedBy().one().string('+')
 			.followedBy().one().ruleset('sum', 'rvalue')
 			.transform((node) => {
-				return parseInt(node.children['lvalue']) + parseInt(node.children['rvalue']);
+				return new Node(
+					'MATCH',
+					(parseInt(node.children.lvalue[0].raw) + parseInt(node.children.rvalue[0].raw)).toString(),
+					{},
+					[node.children.lvalue[0], node.children.rvalue[1]]
+				);
 			}),
-		Rule.match().one().regexp('[0-9]+', 'value')
-			.transform((node) => {
-				return parseInt(node.children['value'])
-			}),
+		Rule.match().one().regexp('[0-9]+', 'value'),
 		Rule.throw('Expected an Integer')
 	])
 });
