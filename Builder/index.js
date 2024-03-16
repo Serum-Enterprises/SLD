a4eca0fac1d5aabbf53ade9ee1f20b7bbaeb966d
philipp <philipp@steckenbauer.com>
2024-03-16 14:11:36 +0400

    Updated Builder Exports

--- Builder/index.js
+++ Builder/index.js
@@ -2,4 +2,15 @@ const { Rule, QuantitySelector, SymbolSelector } = require('./src/Rule');
 const { Grammar } = require('./src/Grammar');
 const { RuleSet } = require('./src/RuleSet');
 
-module.exports = { Grammar, RuleSet, Rule, QuantitySelector, SymbolSelector };
+const { SymbolSet, BaseSymbol, Node } = require('../Parser');
+
+module.exports = {
+	Grammar,
+	RuleSet,
+	Rule,
+	SymbolSet,
+	BaseSymbol,
+	Node,
+	QuantitySelector,
+	SymbolSelector
+};
